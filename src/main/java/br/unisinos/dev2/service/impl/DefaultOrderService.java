09461514a7f96480fe2b27ae4be657a0a7efb93a
Derick Rentz <derick.rentz@sap.com>
2020-04-28 15:00:36 -0300

    Creating order on separate method and setting order status

--- src/main/java/br/unisinos/dev2/service/impl/DefaultOrderService.java
+++ src/main/java/br/unisinos/dev2/service/impl/DefaultOrderService.java
@@ -5,6 +5,11 @@ import br.unisinos.dev2.model.CartModel;
 import br.unisinos.dev2.model.OrderModel;
 import br.unisinos.dev2.service.CartService;
 import br.unisinos.dev2.service.OrderService;
+import br.unisinos.dev2.states.order.OpenOrder;
+import br.unisinos.dev2.states.order.OrderState;
+import br.unisinos.dev2.states.order.SentOrder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -14,15 +19,29 @@ public class DefaultOrderService implements OrderService {
     @Resource
     private CartService cartService;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultOrderService.class);
+
     @Resource
     private ModelFactory modelFactory;
 
-    @Override
-    public OrderModel placeOrder() {
+    private OrderState orderState;
+
+    protected OrderModel createOrder() {
         try {
             OrderModel orderModel = modelFactory.create(OrderModel.class);
-            CartModel cart = cartService.getCurrentCart();
+            orderState = new OpenOrder(orderModel);
+            return orderModel;
+        } catch (Exception e) {
+            LOGGER.error("Error creating order. Returning null");
+        }
+        return null;
+    }
 
+    @Override
+    public OrderModel placeOrder() {
+        OrderModel orderModel = createOrder();
+        try {
+            CartModel cart = cartService.getCurrentCart();
             orderModel.setCode("123412");
             orderModel.setCurrency("BRL");
             orderModel.setDeliveryAddress(cart.getUser().getAddress());
@@ -30,6 +49,8 @@ public class DefaultOrderService implements OrderService {
             orderModel.setOwner(cart.getUser());
             orderModel.setPaymentInfo(cart.getPaymentInfo());
             orderModel.setProducts(cart.getProducts());
+            orderState.gotSent();
+
             return orderModel;
         } catch (Exception exception) {
             System.out.println("Error during place order");
