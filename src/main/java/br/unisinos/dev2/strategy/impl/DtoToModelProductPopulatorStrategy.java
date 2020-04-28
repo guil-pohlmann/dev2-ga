9761ff871d6f85437c1cda09003aafcb63cc5fb9
Derick Rentz <derick.rentz@sap.com>
2020-04-28 15:01:06 -0300

    Creating populator from dto to model for products

--- src/main/java/br/unisinos/dev2/strategy/impl/DtoToModelProductPopulatorStrategy.java
+++ src/main/java/br/unisinos/dev2/strategy/impl/DtoToModelProductPopulatorStrategy.java
@@ -1,4 +1,19 @@
 package br.unisinos.dev2.strategy.impl;
 
-public class DtoToModelProductPopulatorStrategy {
+import br.unisinos.dev2.dto.ProductDTO;
+import br.unisinos.dev2.model.ProductModel;
+import br.unisinos.dev2.strategy.PopulatorStrategy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DtoToModelProductPopulatorStrategy implements PopulatorStrategy<ProductDTO, ProductModel> {
+
+    @Override
+    public void populate(ProductDTO source, ProductModel target) {
+        target.setCode(source.getCode());
+        target.setDescription(source.getDescription());
+        target.setName(source.getName());
+        target.setPrice(source.getPrice());
+        target.setUpc(source.getUpc());
+    }
 }
