c56add07924f620c61a22e50fa822023bdda072e
Derick Rentz <derick.rentz@sap.com>
2020-04-28 17:40:49 -0300

    Implementing test to strategy context

--- src/test/java/br/unisinos/dev2/context/impl/DefaultConverterContextTest.java
+++ src/test/java/br/unisinos/dev2/context/impl/DefaultConverterContextTest.java
@@ -1,16 +1,58 @@
 package br.unisinos.dev2.context.impl;
 
+import br.unisinos.dev2.context.ConverterContext;
 import br.unisinos.dev2.dto.OrderDTO;
+import br.unisinos.dev2.dto.ProductDTO;
+import br.unisinos.dev2.factory.DTOFactory;
 import br.unisinos.dev2.model.OrderModel;
+import br.unisinos.dev2.model.ProductModel;
+import br.unisinos.dev2.strategy.PopulatorStrategy;
 import br.unisinos.dev2.strategy.impl.ModelToDTOOrderPopulatorStrategy;
+import br.unisinos.dev2.strategy.impl.ModelToDTOProductPopulatorStrategy;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.event.annotation.BeforeTestExecution;
+
+import javax.annotation.Resource;
+
+import static org.junit.Assert.assertEquals;
 
 class DefaultConverterContextTest {
 
+    @Resource
+    private ConverterContext defaultConverterContext;
+
+    @Resource
+    private DTOFactory dtoFactory;
+
+    private static final String PRODUCT_CODE = "PRODUCT_CODE";
+    private static final String PRODUCT_DESCRIPTION = "PRODUCT_DESC";
+    private static final String PRODUCT_NAME = "PRODUCT_NAME";
+    private static final String PRODUCT_UPC = "123123123123";
+    private static final double PRODUCT_PRICE = 123.12;
+
     @Test
-    void convert() {
-        DefaultConverterContext defaultConverterContext = new DefaultConverterContext();
-        defaultConverterContext.setPopulatorStrategy(new ModelToDTOOrderPopulatorStrategy());
-        defaultConverterContext.convert(new OrderModel(), OrderDTO.class);
+    void shouldConvertProductModelToProductDtoWhenUsingProductPopulatorStrategy() {
+        defaultConverterContext.setPopulatorStrategy(new ModelToDTOProductPopulatorStrategy());
+        ProductModel productModel = mockProductModel();
+        ProductDTO productDTO = defaultConverterContext.convert(productModel, ProductDTO.class);
+
+        assertEquals(productModel.getCode(), productDTO.getCode());
+        assertEquals(productModel.getDescription(), productDTO.getDescription());
+        assertEquals(productModel.getName(), productDTO.getName());
+        assertEquals(productModel.getUpc(), productDTO.getUpc());
+        assertEquals(productModel.getPrice(), productDTO.getPrice());
+    }
+
+    ProductModel mockProductModel(){
+        ProductModel productModel = new ProductModel();
+        productModel.setCode(PRODUCT_CODE);
+        productModel.setDescription(PRODUCT_DESCRIPTION);
+        productModel.setName(PRODUCT_NAME);
+        productModel.setPrice(PRODUCT_PRICE);
+        productModel.setUpc(PRODUCT_UPC);
+
+        return productModel;
     }
 }
