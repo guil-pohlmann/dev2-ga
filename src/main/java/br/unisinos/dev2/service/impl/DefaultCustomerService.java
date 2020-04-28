16b0ba47ccb4e0bf434396beed08927f9282cde6
Derick Rentz <derick.rentz@sap.com>
2020-04-28 15:00:15 -0300

    Using logger instead sysout

--- src/main/java/br/unisinos/dev2/service/impl/DefaultCustomerService.java
+++ src/main/java/br/unisinos/dev2/service/impl/DefaultCustomerService.java
@@ -3,6 +3,8 @@ package br.unisinos.dev2.service.impl;
 import br.unisinos.dev2.factory.ModelFactory;
 import br.unisinos.dev2.model.CustomerModel;
 import br.unisinos.dev2.service.CustomerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -11,6 +13,8 @@ import static java.util.Objects.nonNull;
 
 @Component
 public class DefaultCustomerService implements CustomerService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultCustomerService.class);
 
     private CustomerModel currentUser;
 
@@ -25,7 +29,7 @@ public class DefaultCustomerService implements CustomerService {
             currentUser = modelFactory.create(CustomerModel.class);
             return currentUser;
         } catch (Exception e) {
-            System.out.println("Something went wrong");
+            LOGGER.error("Error creating a {} using {}. Returning null", CustomerModel.class, ModelFactory.class);
             return null;
         }
     }
