b3c70a0b393e25c9daade5ce0293d15c65652726
Derick Rentz <derick.rentz@sap.com>
2020-04-28 10:04:05 -0300

    Adding method to convert list of model

--- src/main/java/br/unisinos/dev2/context/ConverterContext.java
+++ src/main/java/br/unisinos/dev2/context/ConverterContext.java
@@ -2,9 +2,18 @@ package br.unisinos.dev2.context;
 
 import br.unisinos.dev2.dto.AbstractDTO;
 import br.unisinos.dev2.model.AbstractModel;
+import br.unisinos.dev2.strategy.PopulatorStrategy;
+
+import java.util.List;
 
 public interface ConverterContext {
 
     AbstractDTO convert(AbstractModel source);
 
-}
+    List<AbstractDTO> convertAll(List<AbstractModel> source);
+
+    void setPopulatorStrategy(PopulatorStrategy populatorStrategy);
+
+    void setTarget(AbstractDTO target);
+
+    }
