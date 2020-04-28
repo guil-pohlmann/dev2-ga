e2b16ee6764acc0a2286c944c00bf3b84662c9ab
Derick Rentz <derick.rentz@sap.com>
2020-04-28 14:58:30 -0300

    Creating facade to commerce

--- src/main/java/br/unisinos/dev2/facades/CommerceFacade.java
+++ src/main/java/br/unisinos/dev2/facades/CommerceFacade.java
@@ -1,4 +1,16 @@
 package br.unisinos.dev2.facades;
 
+import br.unisinos.dev2.dto.CartDTO;
+import br.unisinos.dev2.dto.CustomerDTO;
+import br.unisinos.dev2.dto.OrderDTO;
+import br.unisinos.dev2.dto.ProductDTO;
+
 public interface CommerceFacade {
+
+    CartDTO addProductToCart(ProductDTO productDTO);
+
+    OrderDTO placeOrder();
+
+    CustomerDTO getCurrentCustomer();
+
 }
