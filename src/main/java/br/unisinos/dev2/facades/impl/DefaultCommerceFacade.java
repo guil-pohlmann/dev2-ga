c086834c5b52577eb77de774587afeff2588c58e
Derick Rentz <derick.rentz@sap.com>
2020-04-28 14:59:32 -0300

    Creating facade to commerce

--- src/main/java/br/unisinos/dev2/facades/impl/DefaultCommerceFacade.java
+++ src/main/java/br/unisinos/dev2/facades/impl/DefaultCommerceFacade.java
@@ -1,19 +1,23 @@
-package br.unisinos.dev2.facades;
+package br.unisinos.dev2.facades.impl;
 
 import br.unisinos.dev2.context.ConverterContext;
 import br.unisinos.dev2.dto.CartDTO;
+import br.unisinos.dev2.dto.CustomerDTO;
 import br.unisinos.dev2.dto.OrderDTO;
 import br.unisinos.dev2.dto.ProductDTO;
+import br.unisinos.dev2.facades.CommerceFacade;
 import br.unisinos.dev2.service.CartService;
+import br.unisinos.dev2.service.CustomerService;
 import br.unisinos.dev2.service.OrderService;
 import br.unisinos.dev2.strategy.impl.ModelToDTOCartPopulatorStrategy;
+import br.unisinos.dev2.strategy.impl.ModelToDTOCustomerPopulatorStrategy;
 import br.unisinos.dev2.strategy.impl.ModelToDTOOrderPopulatorStrategy;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
 @Component
-public class DefaultCommerceFacade implements CommerceFacade{
+public class DefaultCommerceFacade implements CommerceFacade {
 
     @Resource
     private CartService cartService;
@@ -23,6 +27,9 @@ public class DefaultCommerceFacade implements CommerceFacade{
 
     @Resource
     private ConverterContext converterContext;
+
+    @Resource
+    private CustomerService customerService;
 
     @Override
     public CartDTO addProductToCart(ProductDTO productDTO){
@@ -35,6 +42,11 @@ public class DefaultCommerceFacade implements CommerceFacade{
     public OrderDTO placeOrder(){
         converterContext.setPopulatorStrategy(new ModelToDTOOrderPopulatorStrategy());
         return converterContext.convert(orderService.placeOrder(), OrderDTO.class);
+    }
+
+    public CustomerDTO getCurrentCustomer(){
+        converterContext.setPopulatorStrategy(new ModelToDTOCustomerPopulatorStrategy());
+        return converterContext.convert(customerService.getCurrentUser(), CustomerDTO.class);
     }
 
 }
